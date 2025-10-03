4db98bc250d8a7286130c3832e5504ae9f0ae922
sebasssperez31-beep <sebasss.perez31@gmail.com>
2025-10-03 02:37:39 -0500

    Cambio para que app use puerto dinámico en Render

--- backend/app.py
+++ backend/app.py
@@ -453,5 +453,6 @@ def home():
 
 # ------------------------
 if __name__ == "__main__":
-    print("Backend iniciado en http://0.0.0.0:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Backend iniciado en http://0.0.0.0:{port}")
+    app.run(host="0.0.0.0", port=port, debug=True)
